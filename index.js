c5c432f9ac9052bd4c227b7176634316cd6c33e6
Joshua Cole <joshuafcole@gmail.com>
2013-11-28 01:40:05 -0800

    Drops to depth 0 search

--- index.js
+++ index.js
@@ -9,68 +9,123 @@ var claire = module.exports = {
 };
 
 claire.find = function(term, root, callback, opts) {
-  var terms = term.split('/');
+  if(root[root.length - 1] !== '/') {
+    root += '/';
+  }
   opts = opts || {};
 
   var skipDirs = opts.filters || claire.defaultFilters;
   var matches = [];
   var finder = findit(root);
 
+  var matchOpts = {pre: opts.pre, post: opts.post};
   finder.on('directory', function(dir, stat, stop) {
-
     if(_.contains(skipDirs, path.basename(dir))) {
       return stop();
     }
 
-    var dirs = dir.split('/');
-    if(dirs.length > terms.length) {
-      return stop();
+    var relative = dir.slice(root.length);
+    var match = fuzzy.match(term, relative, matchOpts);
+    if(match) {
+      match.dir = dir;
+      match.file = '';
+      matches.push(match);
     }
 
-    var tail = dirs.length - 1;
-    if(!fuzzy.match(terms[tail], dirs[tail]) && dirs[tail]) {
+    // Depth 0
+    if(relative.length) {
       return stop();
-   }
+    }
   });
 
   finder.on('file', function(file, stat) {
-    var matchOpts = {pre: opts.pre, post: opts.post};
-    var match = fuzzy.match(term, file, matchOpts)
+
+    var match = fuzzy.match(term, file, matchOpts);
     if(!match) {
       return;
     }
 
-    match.file = file;
-
-    if(opts.short) {
-      var i = 0;
-      var lastSlash = 0;
-      for(i = 0; i < file.length; i++) {
-        if(file[i] != term[i]) {
-          break;
-        }
-        if(i && file[i] == '/') {
-          lastSlash = i + 1;
-        }
-      }
-
-      match.short = file.slice(lastSlash);
-
-      if(lastSlash) {
-        match.rendered = fuzzy.match(term.slice(lastSlash), file.slice(lastSlash), matchOpts).rendered;
-      }
-    }
+    match.dir = path.dirname(file);
+    match.file = path.basename(file);
 
     matches.push(match);
   });
 
   finder.on('end', function(err) {
-    console.log('DONE');
+    matches = _.sortBy(matches, function(match) {
+      return -match.score;
+    });
+
+
+    _.each(matches, function(match) {
+      // Slice off shared path between search term and directory.
+      if(opts.short) {
+        var i = 0;
+        var cutoff = 0;
+
+        var relative = match.dir.slice(root.length);
+        while(i < relative.length) {
+          if(relative[i] !== term[i]) {
+            break;
+          }
+
+          if(term[i] === '/') {
+            cutoff = i + 1;
+          }
+          i++;
+        }
+        if(i === relative.length) {
+          cutoff = i;
+        }
+        match.dir = relative.slice(cutoff);
+        match.shared = root + relative.slice(0, cutoff);
+      }
+
+      // Normalize directories.
+      if(match.dir && match.dir[match.dir.length - 1] != '/') {
+        match.dir += '/';
+      }
+    });
+
     callback(err, matches);
   });
-}
+};
 
 module.exports = claire;
 
 // Ex.:
-claire.find('/Users/jcole/re/p/cl/ke', '/Users', console.log, {short: true});
+claire.find('c', '/home/josh/repos/phobos/', function(err, matches) {
+  if(err) {
+    console.log('ERROR:', err);
+  }
+
+  _.each(matches, function(match) {
+    console.log(match.shared, '|', match.dir + match.file, match.score);
+  });
+}, {short: true});
+
+
+
+function getRoot(term, opts) {
+  var root = '';
+  var parts = term.split(path.sep);
+  for(var i = 0; i < parts.length; i++) {
+    try {
+      var tryPath = path.join(root, parts[i]);
+      var stat = fs.statSync(tryPath);
+      if(!stat.isDir()) {
+        break;
+      }
+    } catch (e) {
+      break;
+    }
+  }
+  return root;
+}
+
+function find2(term, callback, opts) {
+  opts = opts || {};
+  var root = getRoot(term, opts);
+
+
+}
